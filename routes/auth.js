e31141ee059ec2dc3fdaf896662750722c09ccb6
Maik Schmidt <maikschmidt91@gmail.com>
2023-02-07 11:53:53 +0100

    Added links between different pages + changed path to -auth-

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,11 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/User.model");
 
 /* GET home page */
-router.get("/signup", (req, res, next) => {
-  res.render("signup/signup", {error: ""});
+router.get("/auth/signup", (req, res, next) => {
+  res.render("auth/signup", {error: ""});
 });
 
-router.post("/signup", async (req, res, next) => {
+router.post("/auth/signup", async (req, res, next) => {
   const newUser = {...req.body};
   newUser.passwordHash = bcrypt.hashSync(newUser.password, bcrypt.genSaltSync(13));
   delete req.body.password;
@@ -21,10 +21,14 @@ router.post("/signup", async (req, res, next) => {
     const err = error.message.split(" ")[0];
     switch(err) {
       case "E11000":
-        res.render("signup/signup", {error: "Error! This user already exists! Please choose a different Username."})
+        res.render("auth/signup", {error: "Error! This user already exists! Please choose a different Username."})
         break;
     };
   }
+});
+
+router.get("/auth/login", (req, res, next) => {
+  res.render("auth/login", {error: ""});
 });
 
 module.exports = router;
